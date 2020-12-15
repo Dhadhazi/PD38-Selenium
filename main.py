ff601df7ec0e5eb44690b01322484906e73e8c8e
Dhadhazi <dhadhazi@gmail.com>
2020-12-15 10:59:19 +0100

    Cookie clicker bot

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "/Users/dani/Documents/chromedriver"
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.python.org/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-li_elements = driver.find_elements_by_css_selector(".event-widget li")
+cookie = driver.find_element_by_id("cookie")
 
-event_times = {}
 
-for i in range(len(li_elements)):
-    time = li_elements[i].find_element_by_tag_name("time")
-    name = li_elements[i].find_element_by_tag_name("a")
-    event_times[i] = {"time": time.text, "name": name.text}
+def get_biggest_upgrade():
+    store = driver.find_elements_by_css_selector("#store div")
+    store.reverse()
+    for item in store:
+        if item.get_attribute("class") != "grayed":
+            item.click()
+            break
 
-print(event_times)
 
-driver.quit()
+timeout = time.time() + 60*5
+click_counter = 0
+while True:
+    click_counter += 1
+    if click_counter > 100:
+        get_biggest_upgrade()
+        click_counter = 0
+    cookie.click()
+    if time.time() > timeout:
+        break
+
+cookies_per_second = driver.find_element_by_id("cps")
+print(cookies_per_second.text)
