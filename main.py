7c15108a4a03afcbcd2b23f5f46f58b40a7624d5
Dhadhazi <dhadhazi@gmail.com>
2020-12-15 09:47:30 +0100

    Python event grapper

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from selenium import webdriver
+
 chrome_driver_path = "/Users/dani/Documents/chromedriver"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.python.org/")
+
+li_elements = driver.find_elements_by_css_selector(".event-widget li")
+
+event_times = {}
+
+for i in range(len(li_elements)):
+    time = li_elements[i].find_element_by_tag_name("time")
+    name = li_elements[i].find_element_by_tag_name("a")
+    event_times[i] = {"time": time.text, "name": name.text}
+
+print(event_times)
+
+driver.quit()
